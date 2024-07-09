3dd1b3b283ee6d72eed50dd225fd3d7103a1c25c
Zahar Litvinchuk <zahar@raynelz.ru>
2024-07-09 23:22:30 +0300

    Initial Commit

---
+++ SUINavigationTest/SUINavigationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUINavigationTestApp.swift
+//  SUINavigationTest
+//
+//  Created by Захар Литвинчук on 09.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SUINavigationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
